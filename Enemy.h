bcef429f5bfaafdd5b5fff8db6737c2b202f2f9f
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2022-06-06 15:25:37 +0900

    3_5完成

--- Enemy.h
+++ Enemy.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Model.h"
 #include "WorldTransform.h"
+#include "DebugText.h"
 
 /// <summary>
 /// 敵
@@ -11,14 +12,41 @@ public:
 	Enemy();
 	~Enemy();
 
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="position">初期座標</param>
+	/// /// <param name="velocity">速度</param>
+	void Initialize(Model* model, const Vector3& position, const Vector3& velocity);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="viewProjection">ビュープロジェクション</param>
+	void Draw(const ViewProjection& viewProjection);
+
 private:
+
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	//3Dモデル
+	Model* model_ = nullptr;
+
+	//速度
+	Vector3	Velocity_;
+
+	DebugText* debugText_ = nullptr;
 
 };
 
-Enemy::Enemy()
-{
-}
 
-Enemy::~Enemy()
-{
-}
