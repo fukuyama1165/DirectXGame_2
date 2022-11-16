7ed317ea1d393000d9518dc5f10383216d55d244
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2022-11-16 17:14:25 +0900

    イージング入れた

---
+++ easing.h
@@ -0,0 +1,23 @@
+#pragma once
+
+double lerp(double a,double b,double t);
+
+double easeInSine(double start, double end, double time);
+
+double easeOutSine(double start, double end, double time);
+
+double easeInOutSine(double start, double end, double time);
+
+double easeOutQuad(double start, double end, double time);
+
+double easeInQuint(double start, double end, double time);
+
+double easeOutQuint(double start, double end, double time);
+
+double easeInOutQuint(double start, double end, double time);
+
+double easeInBack(double start, double end, double time);
+
+double easeOutBack(double start, double end, double time);
+
+double easeOutElastic(double start, double end,double time);
